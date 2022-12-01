5d94f48cfed4a93e99094f4be81208e7bf47243d
dev7k <dev7k@proton.me>
2022-12-01 18:18:21 +0100

    Added service for Platform Repository

--- src/PlatformService/Program.cs
+++ src/PlatformService/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
